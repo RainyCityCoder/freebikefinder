f9f490cb19f8ad903c83a6ba179180fdd4639f7f
RainyCityDiver <cvmembership@posteo.net>
2023-04-03 15:21:03 -0700

    Working to get API connected to new Mongo acct..

--- dataInterface/shops.js
+++ dataInterface/shops.js
@@ -1,9 +1,11 @@
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 
+const mongoDBPassword = process.env.PASSWORD;
+const mongoDBPORT = process.env.PORT;
 // uri is from mongodb account > Connect > Connect your app > Driver: Node
-const uri =
-  'mongodb+srv://superuser:4IYc1f1LQtmvDELv@cluster0.mwyfrof.mongodb.net/?retryWrites=true&w=majority';
+const uri = 
+  `mongodb+serv://admin:${mongoDBPassword}@cluster0.${mongoDBPORT}.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri);
 
